b171e9465c748813572ede6c36999f87dad33856
umesh <umesharma.us11@gmail.com>
2023-01-18 16:32:40 +0530

    Setting up the fronend with create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-amber-200 text-5xl">Hello World</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
