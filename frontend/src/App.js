4d99a72c352aa0f1dc42b3d7db55b45418443d34
04harshavardhan <04harshavardhan@gmail.com>
2023-01-22 22:32:01 +0530

    Sorry i edited app.js by mistake

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
 
-import Filters from "./components/filters";
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <h1 className="text-amber-200 text-5xl">Hello World</h1> */}
-        <Filters />
+        <h1 className="text-amber-200 text-5xl">Hello World</h1>
       </header>
     </div>
   );
