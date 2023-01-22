3c3115bd7731bea33b77b7e8089af2dcaf4ae42e
04harshavardhan <04harshavardhan@gmail.com>
2023-01-22 22:09:33 +0530

    Created location select component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import "./App.css";
+
+import Filters from "./components/filters";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-amber-200 text-5xl">Hello World</h1>
+        {/* <h1 className="text-amber-200 text-5xl">Hello World</h1> */}
+        <Filters />
       </header>
     </div>
   );
