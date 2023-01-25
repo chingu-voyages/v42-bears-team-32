fa88ae6e9bd8b3432b3e1a2fe99d09c89069f479
ayenyeinchanpyae <57614389+ayenyeinchanpyae@users.noreply.github.com>
2023-01-25 21:13:15 +0630

    added today offer, best cuisine and suggested for you section

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ExploreCategory from "../Components/Category/ExploreCategory";
-import TodayOffer from "../Components/ProductCard/TodayOffer";
+import HomeProducts from "../Components/ProductCard/HomeProducts";
+import { todayOfferData } from "../constants/data.js";
+import { bestCuisineData } from "../constants/data.js";
+import { suggestedFoodData } from "../constants/data.js";
 
 function Home() {
   return (
     <div>
       <p className="px-[150px]">Hero Section</p>
       <ExploreCategory />
-      <TodayOffer />
+      <HomeProducts data={todayOfferData} label="Today's Offer" />
+      <HomeProducts data={bestCuisineData} label="Best Cuisine" />
+      <HomeProducts data={suggestedFoodData} label="Suggested For You" />
     </div>
   );
 }
