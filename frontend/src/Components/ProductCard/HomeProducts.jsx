895b1c445f03c517e4d4b0dd0dcbaf738b23f52d
ayenyeinchanpyae <57614389+ayenyeinchanpyae@users.noreply.github.com>
2023-01-25 21:16:04 +0630

    added best cuisine and suggested section

--- frontend/src/Components/ProductCard/HomeProducts.jsx
+++ frontend/src/Components/ProductCard/HomeProducts.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import HomeProductCard from "./HomeProductCard";
 function HomeProducts({ data, label }) {
   return (
-    <div className="px-[150px] py-[25px]">
+    <div className="px-[150px] py-[35px]">
       <p className="text-dark-2 text-h1 font-weight-700 py-[25px]">{label}</p>
 
       <div className="flex justify-between">
