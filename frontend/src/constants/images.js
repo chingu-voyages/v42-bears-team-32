f8fdf1921c6b37636db640bd241c920384cdd48d
umesh <umesharma.us11@gmail.com>
2023-01-29 09:40:43 +0530

    Created singleproductCommponent and added a rating component to it

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,8 +1,14 @@
 import logo from '../asset/logo.svg';
 import cart from '../asset/cart-icon.svg';
+import food from '../asset/food.svg';
+import backarrow from '../asset/back-arrowbtn.svg';
+import ellipse from '../asset/Ellipse.svg';
 
 const image = {
   logo,
   cart,
+  food,
+  backarrow,
+  ellipse,
 };
 export default image;
